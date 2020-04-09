fa9146f686a6391a01622748dcfb01eda734b173
Dave Willenberg <dave@detroit-english.de>
2020-04-09 20:45:47 +0200

    Allow use as imported module and output items as JSON

--- src/index.js
+++ src/index.js
@@ -1,102 +1,124 @@
 #! /usr/bin/env node
 
 require('colors')
-const helper = require('./helper')
 const fs = require('fs')
 const argv = require('commander')
 const ProgressBar = require('progress')
 const dateFormat = require('dateformat')
 const ffmpeg = require('fluent-ffmpeg')
 const { version } = require('../package.json')
-let seedFile
 
-argv
-  .version(version, '-v, --version')
-  .name('iptv-checker')
-  .description(
-    'Utility to check .m3u playlists entries. If no file path or url is provided, this program will attempt to read stdin'
-  )
-  .usage('[options] [file-or-url]')
-  .option('-o, --output [output]', 'Path to output directory')
-  .option(
-    '-t, --timeout [timeout]',
-    'Set the number of milliseconds for each request',
-    60000
-  )
-  .option('-k, --insecure', 'Allow insecure connections when using SSL', false)
-  .option('-d, --debug', 'Toggle debug mode')
-  .action(function (file = null) {
-    seedFile = file
-  })
-  .parse(process.argv)
+const isMain = require.main === module
+let seedFile, onlineFile, offlineFile, duplicatesFile, bar
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = !+argv.insecure
-
-const outputDir =
-  argv.output || `iptv-checker-${dateFormat(new Date(), 'd-m-yyyy-hh-MM-ss')}`
-const onlineFile = `${outputDir}/online.m3u`
-const offlineFile = `${outputDir}/offline.m3u`
-const duplicatesFile = `${outputDir}/duplicates.m3u`
-
-const config = {
-  debug: argv.debug,
-  timeout: parseInt(argv.timeout),
-}
-
-const debugLogger = helper.debugLogger(config.debug)
-
-debugLogger('Configuration:', config)
-
-try {
-  fs.lstatSync(outputDir)
-} catch (e) {
-  fs.mkdirSync(outputDir)
-}
-
-fs.writeFileSync(onlineFile, '#EXTM3U\n')
-fs.writeFileSync(offlineFile, '#EXTM3U\n')
-fs.writeFileSync(duplicatesFile, '#EXTM3U\n')
-
-let bar
-let stats = {
-  total: 0,
-  online: 0,
-  offline: 0,
-  duplicates: 0,
-}
-
-init()
-
-async function init() {
-  debugLogger(`Checking...`)
+if (isMain) {
+  argv
+    .version(version, '-v, --version')
+    .name('iptv-checker')
+    .description(
+      'Utility to check .m3u playlists entries. If no file path or url is provided, this program will attempt to read stdin'
+    )
+    .usage('[options] [file-or-url]')
+    .option('-o, --output [output]', 'Path to output directory')
+    .option(
+      '-t, --timeout [timeout]',
+      'Set the number of milliseconds for each request',
+      60000
+    )
+    .option(
+      '-k, --insecure',
+      'Allow insecure connections when using SSL',
+      false
+    )
+    .option('-d, --debug', 'Toggle debug mode')
+    .action(function (file = null) {
+      seedFile = file
+    })
+    .parse(process.argv)
+  const outputDir =
+    argv.output || `iptv-checker-${dateFormat(new Date(), 'd-m-yyyy-hh-MM-ss')}`
+  onlineFile = `${outputDir}/online.m3u`
+  offlineFile = `${outputDir}/offline.m3u`
+  duplicatesFile = `${outputDir}/duplicates.m3u`
 
   try {
-    console.time('Execution time')
+    fs.lstatSync(outputDir)
+  } catch (e) {
+    fs.mkdirSync(outputDir)
+  }
 
-    let playlist = await helper.parsePlaylist(seedFile)
+  fs.writeFileSync(onlineFile, '#EXTM3U\n')
+  fs.writeFileSync(offlineFile, '#EXTM3U\n')
+  fs.writeFileSync(duplicatesFile, '#EXTM3U\n')
+}
 
-    stats.total = playlist.items.length
+async function init(
+  src = seedFile,
+  {
+    timeout = +argv.timeout || 60000,
+    insecure = argv.insecure || false,
+    debug = argv.debug || false,
+  }
+) {
+  let items
+  const stats = {
+    total: 0,
+    online: 0,
+    offline: 0,
+    duplicates: 0,
+  }
+  const config = {
+    debug,
+    timeout,
+    insecure,
+    isMain,
+  }
+  const helper = require('./helper')
 
-    bar = new ProgressBar(':bar', { total: stats.total })
+  const { addToCache, checkCache, parseMessage } = helper
 
-    for (let item of playlist.items) {
-      if (!config.debug) {
+  const writeToFile = helper.writeToFile.bind(config)
+  const debugLogger = helper.debugLogger(debug)
+
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = !+insecure
+
+  console.time('Execution time')
+
+  try {
+    if (!Array.isArray(src)) {
+      ;({ items } = await helper.parsePlaylist(src))
+    } else {
+      items = src
+    }
+
+    stats.total = items.length
+
+    debugLogger(`Checking ${stats.total} items...`)
+
+    bar = isMain && new ProgressBar(':bar', { total: stats.total })
+
+    let checkedItems = []
+
+    for (let item of items) {
+      if (!debug && !isMain) {
         bar.tick()
       }
 
       if (!item.url) continue
 
-      if (helper.checkCache(item.url)) {
-        helper.writeToFile(duplicatesFile, item)
+      if (checkCache(item)) {
+        writeToFile(duplicatesFile, item)
 
         stats.duplicates++
 
         continue
       }
 
-      helper.addToCache(item.url)
+      addToCache(item)
 
-      await validateStatus(item, item.url)
+      let checkedItem = await validateStatus(item)
+
+      if (!isMain) checkedItems.push(checkedItem)
     }
 
     if (config.debug) {
@@ -110,37 +132,56 @@ async function init() {
       `Duplicates: ${stats.duplicates}`.yellow,
     ].join('\n')
 
-    console.log(`\n${result}`)
-
-    process.exit(0)
+    if (isMain) {
+      console.log(`\n${result}`)
+      return process.exit(0)
+    } else {
+      return checkedItems
+    }
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    if (isMain) {
+      console.error(err)
+      process.exit(1)
+    } else throw err
   }
-}
 
-function validateStatus(parent, currentUrl) {
-  return new Promise(resolve => {
-    ffmpeg(currentUrl, { timeout: parseInt(config.timeout / 1000) }).ffprobe(
-      function (err) {
+  function validateStatus(item) {
+    let { url } = item
+    return new Promise(resolve => {
+      ffmpeg(url, { timeout: parseInt(timeout / 1000) }).ffprobe(function (
+        err,
+        metadata
+      ) {
+        let result = {
+          ...item,
+          status: { ok: null, checked: Date.now(), timeout, metadata },
+        }
         if (err) {
-          const message = String(helper.parseMessage(err, currentUrl))
+          result.status.ok = false
+          const message = String(parseMessage(err, url))
+          result.status.reason = message
 
-          helper.writeToFile(offlineFile, parent, message)
+          writeToFile(offlineFile, item, message)
 
-          debugLogger(`${currentUrl} (${message})`.red)
+          debugLogger(`${url} (${message})`.red)
 
           stats.offline++
         } else {
-          debugLogger(`${currentUrl}`.green)
+          result.status.ok = true
+          debugLogger(`${url}`.green)
 
-          helper.writeToFile(onlineFile, parent)
+          writeToFile(onlineFile, item)
 
           stats.online++
         }
+        resolve(result)
+      })
+    })
+  }
+}
 
-        resolve()
-      }
-    )
-  })
+if (isMain) {
+  init()
+} else {
+  module.exports = init
 }
